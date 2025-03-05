0e152ee6b44c418bf9053d5c63fbd38544b27930
LenzDev <glnlen69@gmail.com>
2025-03-05 15:40:46 +0700

    Update init.js

--- asset/js/init.js
+++ asset/js/init.js
@@ -10,8 +10,16 @@
       slideshowSpeed: 7000,
       animationSpeed: 600,
       randomize: false
-   });
-});
+ 
+       $(document).ready(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: true,
+        directionNav: false, // Menghilangkan tombol prev/next untuk layar kecil
+        touch: true, // Memastikan dukungan geser (swipe) di Android
+        smoothHeight: true
+    });
+});}
 
 
 
